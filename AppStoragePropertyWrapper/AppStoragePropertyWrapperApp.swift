2fa4d29a860724668ce29b3838b20ac7cb91f392
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-22 10:44:15 +0300

    Initial Commit

---
+++ AppStoragePropertyWrapper/AppStoragePropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoragePropertyWrapperApp.swift
+//  AppStoragePropertyWrapper
+//
+//  Created by ramil on 22.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct AppStoragePropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
